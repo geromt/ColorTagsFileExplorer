444e42f1401e527d037d5b02f9a04c45ac83640d
gero <gerardomartinez205@gmail.com>
2023-08-16 16:45:44 -0600

    feat: Connects prueba_wx.py with the code generated by main_window_ui

--- prueba_wx.py
+++ prueba_wx.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import json
 import os.path
 import subprocess
@@ -5,18 +7,19 @@ import sys
 
 from PyQt5.QtCore import Qt, QItemSelectionModel
 from PyQt5.QtGui import QColor, QContextMenuEvent
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileSystemModel, QVBoxLayout, QWidget, QListView, QTreeView, \
-    QPushButton, QStyledItemDelegate
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileSystemModel, QListView, QStyledItemDelegate
+from main_window_ui import Ui_MainWindow
 
 current_path = ""
 file_states = {}
 
 
-class FileExplorerApp(QMainWindow):
+class FileExplorerApp(QMainWindow, Ui_MainWindow):
     def __init__(self):
         global file_states
         global current_path
         super().__init__()
+        self.setupUi(self)
 
         current_path = os.path.expanduser("~")
         print(current_path)
@@ -24,22 +27,33 @@ class FileExplorerApp(QMainWindow):
             with open("meta.json", "r") as f:
                 file_states = json.loads(f.read())
 
-        self.setWindowTitle("File Explorer")
+        self.setWindowTitle("Color Tag File Explorer")
         self.setGeometry(100, 100, 800, 600)
 
-        layout = QVBoxLayout()
-        self.central_widget = QWidget()
-        self.central_widget.setLayout(layout)
-        self.setCentralWidget(self.central_widget)
-        self.list_view = QListView()
         self.file_model = QFileSystemModel()
         self.list_selection_model = QItemSelectionModel(self.file_model)
 
-        button = QPushButton('Folder Up')
-        button.clicked.connect(self.go_folder_up)
-        layout.addWidget(button)
-
         self.init_ui()
+
+    def init_ui(self):
+        self.file_model.setRootPath(current_path)
+
+        self.folderUpButton.clicked.connect(self.go_folder_up)
+
+        self.listView.setModel(self.file_model)
+        self.listView.setRootIndex(self.file_model.index(current_path))
+        self.listView.doubleClicked.connect(self.open_file)
+
+        self.listView.setDragEnabled(True)
+        self.listView.setDragDropMode(QListView.DragOnly)
+        self.listView.setSelectionMode(QListView.ExtendedSelection)
+        self.listView.setDefaultDropAction(Qt.MoveAction)
+        self.listView.setDropIndicatorShown(True)
+
+        self.listView.setSelectionModel(self.list_selection_model)
+
+        delegate = ColorDelegate(self.list_selection_model, self.file_model, self.listView)
+        self.listView.setItemDelegate(delegate)
 
     def closeEvent(self, event):
         print("Close button or Alt+F4 was pressed.")
@@ -64,34 +78,13 @@ class FileExplorerApp(QMainWindow):
                 opener = "open" if sys.platform == "darwin" else "xdg-open"
                 subprocess.call([opener, new_path])
         else:
-            self.list_view.setRootIndex(self.file_model.index(new_path))
+            self.listView.setRootIndex(self.file_model.index(new_path))
             current_path = new_path
 
     def go_folder_up(self):
         global current_path
         current_path = os.path.dirname(current_path)
-        self.list_view.setRootIndex(self.file_model.index(current_path))
-
-    def init_ui(self):
-        self.file_model.setRootPath(current_path)
-
-        self.list_view.setModel(self.file_model)
-        self.list_view.setRootIndex(self.file_model.index(current_path))
-        self.list_view.doubleClicked.connect(self.open_file)
-
-        self.list_view.setDragEnabled(True)
-        self.list_view.setDragDropMode(QListView.DragOnly)
-        self.list_view.setSelectionMode(QListView.ExtendedSelection)
-        self.list_view.setDefaultDropAction(Qt.MoveAction)
-        self.list_view.setDropIndicatorShown(True)
-
-        self.list_view.setSelectionModel(self.list_selection_model)
-
-        delegate = ColorDelegate(self.list_selection_model, self.file_model, self.list_view)
-        self.list_view.setItemDelegate(delegate)
-
-        layout = self.centralWidget().layout()
-        layout.addWidget(self.list_view)
+        self.listView.setRootIndex(self.file_model.index(current_path))
 
 
 class ColorDelegate(QStyledItemDelegate):
@@ -109,29 +102,24 @@ class ColorDelegate(QStyledItemDelegate):
         self.special_item_index = index
 
     def paint(self, painter, option, index):
-        print("Corriendo paint")
         full_path = os.path.join(current_path, index.data())
         print("Full path: " + full_path)
 
         if full_path in file_states:
             painter.save()
             if file_states[full_path] == 1:
-                print("Pintando en estado 1: " + full_path)
                 painter.fillRect(option.rect, self.special_color)
             elif file_states[full_path] == 2:
-                print("Pintando en estado 2: " + full_path)
                 painter.fillRect(option.rect, self.special_color2)
             elif file_states[full_path] == 0:
-                print("Pintando en estado 0: " + full_path)
                 painter.fillRect(option.rect, self.normal_color)
             painter.restore()
 
-        super().paint(painter, option, index)  # Paint the original content
+        super().paint(painter, option, index)
 
         if index == self.special_item_index:
-            text = index.data(Qt.DisplayRole)
             painter.save()
-            painter.setPen(Qt.black)  # Set text color to black
+            painter.setPen(Qt.black)
             painter.restore()
 
     def createEditor(self, parent, option, index):
